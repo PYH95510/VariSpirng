27a3c1e449fc1a5bdaeb540e2366bef56fad2e7f
Yohan <pyh95510@gmail.com>
2022-11-10 23:56:12 -0500

    dto added for using Naver api

---
+++ src/main/java/thinkoutloud/firstproject/data/dto/MemberDTO.java
@@ -0,0 +1,16 @@
+package thinkoutloud.firstproject.data.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class MemberDTO {
+
+    private String name;
+    private String email;
+    private String organization;
+
+}
